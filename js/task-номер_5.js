dace083d385fbe21a363e2d840e9b6ef88e925a5
Vladimir-Plotnikov <71009993+Vladimir-Plotnikov@users.noreply.github.com>
2021-02-16 00:06:51 +0200

    module5
    module5 theLastCommit

--- js/task-номер_5.js
+++ js/task-номер_5.js
@@ -1,6 +1,6 @@
 class Car {
-    getSpecs(car) {
-        console.log(car);
+    static getSpecs(car) {
+        console.log(`maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance:  ${car.distance}, price: ${car.price}`);
     }
     constructor({ maxSpeed, speed, isOn, distance, price }) {
         this.maxSpeed = maxSpeed;
@@ -15,46 +15,37 @@ class Car {
     set price(newPrice) {
         this._price = newPrice
     }
-
     turnOn() { this.isOn = true }
-    turnOff() { this.isOn != true }
-    
+    turnOff() { this.isOn = false }
     accelerate(value) {
-        if (this.speed + value >= this.maxSpeed) {
+        this.speed = value;
+        if(this.speed + value >= this.maxSpeed) {
             this.speed = this.speed + value;
         }
     }
-
     decelerate(value) {
-        if (this.speed - value >= 0) {
+        this.speed = value;
+        if(this.speed - value >= 0) {
             this.speed = this.speed - value;
         }
     }
-
     drive(hours) {
-        if (this.isOn = true) {
+        if(this.isOn != false) {
             this.distance = hours * this.speed;
         }
     }
 }
-
-
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
-
 mustang.turnOn();
 mustang.accelerate(50);
 mustang.drive(2);
-
 Car.getSpecs(mustang);
 // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
-
 mustang.decelerate(20);
 mustang.drive(1);
 mustang.turnOff();
-
 Car.getSpecs(mustang);
 // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
-
 console.log(mustang.price); // 2000
 mustang.price = 4000;
 console.log(mustang.price); // 4000
